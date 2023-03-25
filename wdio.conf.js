9d1d7ba2ff427955749c0e95a16600be91d87500
luisbrito2900 <luisjahziel1029@gmail.com>
2023-03-25 06:29:29 -0400

    Updated WebdriverIO configuration path

--- wdio.conf.js
+++ wdio.conf.js
@@ -22,7 +22,7 @@ exports.config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ["./test/specs/signUpTest.js"],
+  specs: ["./test/specs/**/*.js"],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
