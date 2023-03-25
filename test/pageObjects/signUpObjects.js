2e0da4774695dc96f63a5ee120165f18a957366c
luisbrito2900 <luisjahziel1029@gmail.com>
2023-03-25 06:06:19 -0400

    Adding implicit waits

--- test/pageObjects/signUpObjects.js
+++ test/pageObjects/signUpObjects.js
@@ -1,21 +1,32 @@
 class SignUpPage {
-    get firstNameInput() { return $('#firstname'); }
-    get lastNameInput() { return $('#lastname'); }
-    get emailInput() { return $('#email_address'); }
-    get passwordInput() { return $('#password'); }
-    get passwordConfirmationInput() { return $('#password-confirmation'); }
+    get firstNameInput() { return $('input[name="firstname"]'); }
+    get lastNameInput() { return $('input[name="lastname"]'); }
+    get emailInput() { return $('input[name="email"]'); }
+    get passwordInput() { return $('input[name="password"]'); }
+    get passwordConfirmationInput() { return $('input[name="password_confirmation"]'); }
     get signUpButton() { return $('button[title="Create an Account"]'); }
 
     open() {
         browser.url('/customer/account/create/');
     }
 
-    signUp(firstName, lastName, email, password, passwordConfirmation) {
+    async signUp(firstName, lastName, email, password, passwordConfirmation) {
+        await this.firstNameInput.waitForExist();
         this.firstNameInput.setValue(firstName);
+
+        await this.lastNameInput.waitForExist();
         this.lastNameInput.setValue(lastName);
+
+        await this.emailInput.waitForExist();
         this.emailInput.setValue(email);
+
+        await this.passwordInput.waitForExist();
         this.passwordInput.setValue(password);
+
+        await this.passwordConfirmationInput.waitForExist();
         this.passwordConfirmationInput.setValue(passwordConfirmation);
+
+        await this.signUpButton.waitForExist();
         this.signUpButton.click();
     }
 }
