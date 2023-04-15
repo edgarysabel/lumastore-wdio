0a585326442937e52a414c13ff23d382553baa0a
luisbrito2900 <luisjahziel1029@gmail.com>
2023-04-15 11:40:58 -0400

    Cleaning code

--- test/pageObjects/signUpObjects.js
+++ test/pageObjects/signUpObjects.js
@@ -29,16 +29,38 @@ class SignUpPage {
     );
   }
 
+  get emptyFieldsError() {
+    return $(
+      '//div[contains(text(),"Thank you for registering with Main Website Store.")]'
+    );
+  }
+
+  get firstNameFieldError() {
+    return $('div[id="firstname-error"]');
+  }
+
+  get lastNameFieldError() {
+    return $('div[id="lastname-error"]');
+  }
+
+  get emailFieldError() {
+    return $('div[id="email_address-error"]');
+  }
+
+  get passwordFieldError() {
+    return $('div[id="email_address-error"]');
+  }
+
   async open() {
     await browser.url("/customer/account/create/");
   }
 
-  async signUp(firstName, lastName, email, password, passwordConfirmation) {
+  async signUp(firstName, lastName, email, password) {
     await this.firstNameInput.setValue(firstName);
     await this.lastNameInput.setValue(lastName);
     await this.emailInput.setValue(email);
     await this.passwordInput.setValue(password);
-    await this.passwordConfirmationInput.setValue(passwordConfirmation);
+    await this.passwordConfirmationInput.setValue(password);
   }
 }
 
--- test/specs/signUpTest.js
+++ test/specs/signUpTest.js
@@ -1,17 +1,13 @@
 const { expect } = require("chai");
-const faker = require("faker");
-const generatePassword = require("../../utils/passwordGenerator");
+const signUpTestData = require("../data/testData");
 const signUpObjects = require("../pageObjects/signUpObjects");
 
 describe("Sign-up Test", () => {
   it("Should register a new user", async () => {
-    const firstName = faker.name.firstName();
-    const lastName = faker.name.lastName();
-    const email = faker.internet.email();
-    const password = generatePassword();
+    const { firstName, lastName, email, password } = signUpTestData;
 
     await signUpObjects.open();
-    await signUpObjects.signUp(firstName, lastName, email, password, password);
+    await signUpObjects.signUp(firstName, lastName, email, password);
     expect(await signUpObjects.firstNameInput.getValue()).to.equal(firstName);
     expect(await signUpObjects.lastNameInput.getValue()).to.equal(lastName);
     expect(await signUpObjects.emailInput.getValue()).to.equal(email);
@@ -26,4 +22,31 @@ describe("Sign-up Test", () => {
       "Thank you for registering with Main Website Store."
     );
   });
+  /*
+  it("Should not allow submission with empty fields", async () => {
+    await signUpObjects.open();
+    await signUpObjects.signUpButton.click();
+
+    expect(await signUpObjects.emptyFieldsError).to.exist;
+  });
+
+  it("Should not allow submission with non-matching passwords", async () => {
+    const firstName = faker.name.firstName();
+    const lastName = faker.name.lastName();
+    const email = faker.internet.email();
+    const password = generatePassword();
+    const confirmPassword = password + "1";
+
+    await signUpObjects.open();
+    await signUpObjects.signUp(
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword
+    );
+    await signUpObjects.signUpButton.click();
+
+    expect(await signUpObjects.nonMatchingPasswordsError).to.exist;
+  });*/
 });
