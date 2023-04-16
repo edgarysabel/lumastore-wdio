5dcadb2909472e89f32ddba60e92a937ee406580
luisbrito2900 <luisjahziel1029@gmail.com>
2023-04-16 00:14:03 -0400

    Code adjust

--- test/pageObjects/signUpObjects.js
+++ test/pageObjects/signUpObjects.js
@@ -28,56 +28,16 @@ class SignUpPage {
       '//div[contains(text(),"Thank you for registering with Main Website Store.")]'
     );
   }
-
-  get emptyFieldsError() {
-    return $(
-      '//div[contains(text(),"Thank you for registering with Main Website Store.")]'
-    );
-  }
-
-  get firstNameFieldError() {
-    return $('div[id="firstname-error"]');
-  }
-
-  get lastNameFieldError() {
-    return $('div[id="lastname-error"]');
-  }
-
-  get emailFieldError() {
-    return $('div[id="email_address-error"]');
-  }
-
-  get passwordFieldError() {
-    return $('div[id="password-error"]');
-  }
-
-  get passwordConfirmationFieldError() {
-    return $('div[id="password-confirmation-error"]');
-  }
-
-  get passwordMismatchError() {
-    return $('//div[contains(text(),"Please enter the same value again.")]');
-  }
-
   async open() {
     await browser.url("/customer/account/create/");
   }
 
-  async signUp(
-    firstName,
-    lastName,
-    email,
-    password,
-    nonMatchingPassword = null
-  ) {
+  async signUp(firstName, lastName, email, password) {
     await this.firstNameInput.setValue(firstName);
     await this.lastNameInput.setValue(lastName);
     await this.emailInput.setValue(email);
     await this.passwordInput.setValue(password);
-    const passwordToConfirm = nonMatchingPassword
-      ? nonMatchingPassword
-      : password;
-    await this.passwordConfirmationInput.setValue(passwordToConfirm);
+    await this.passwordConfirmationInput.setValue(password);
   }
 }
 
