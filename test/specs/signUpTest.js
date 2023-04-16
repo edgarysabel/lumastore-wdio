9e05f1b91495aff00a12425f3577f2a9e704a13f
luisbrito2900 <luisjahziel1029@gmail.com>
2023-04-16 00:04:35 -0400

    Deleting test cases

--- test/specs/signUpTest.js
+++ test/specs/signUpTest.js
@@ -1,19 +1,12 @@
 const { expect } = require("chai");
 const signUpTestData = require("../data/testData");
 const signUpObjects = require("../pageObjects/signUpObjects");
-const { generateUserData } = require("../data/testDataGenerator");
 
 describe("Sign-up Test", () => {
-  beforeEach(async () => {
-    await signUpObjects.open();
-  });
-
-  afterEach(async () => {
-    await browser.reloadSession();
-  });
   it("Should register a new user", async () => {
     const { firstName, lastName, email, password } = signUpTestData;
 
+    await signUpObjects.open();
     await signUpObjects.signUp(firstName, lastName, email, password);
     expect(await signUpObjects.firstNameInput.getValue()).to.equal(firstName);
     expect(await signUpObjects.lastNameInput.getValue()).to.equal(lastName);
@@ -24,38 +17,8 @@ describe("Sign-up Test", () => {
     );
     await signUpObjects.signUpButton.click();
 
-    expect(await signUpObjects.welcomeMessage).to.exist;
     expect(await signUpObjects.welcomeMessage.getText()).to.contain(
       "Thank you for registering with Main Website Store."
-    );
-  });
-
-  it("Should not allow submission with empty fields", async () => {
-    await signUpObjects.signUpButton.click();
-
-    expect(await signUpObjects.firstNameFieldError).to.exist;
-    expect(await signUpObjects.lastNameFieldError).to.exist;
-    expect(await signUpObjects.emailFieldError).to.exist;
-    expect(await signUpObjects.passwordFieldError).to.exist;
-    expect(await signUpObjects.passwordConfirmationFieldError).to.exist;
-  });
-
-  it("Should not allow submission with non-matching passwords", async () => {
-    const { firstName, lastName, email, password } = generateUserData();
-    const nonMatchingPassword = password + "1";
-
-    await signUpObjects.signUp(
-      firstName,
-      lastName,
-      email,
-      password,
-      nonMatchingPassword
-    );
-    await signUpObjects.signUpButton.click();
-
-    expect(await signUpObjects.passwordMismatchError).to.exist;
-    expect(await signUpObjects.passwordMismatchError.getText()).to.contain(
-      "Please enter the same value again."
     );
   });
 });
