99f324837be40eed44aac5d696c50c56bef80c04
luisbrito2900 <luisjahziel1029@gmail.com>
2023-08-04 01:15:45 -0400

    retest

--- test/specs/TC-002.addAddress.uitest.js
+++ test/specs/TC-002.addAddress.uitest.js
@@ -24,7 +24,7 @@ describe("Add address to customer information test", () => {
   });
 
   it("Add address", async () => {
-    await dashboardPage.myAccountMenuDropdown.click();
+    await dashboardPage.myAccountMenuDropdown.click(); //hello
     await dashboardPage.addressBook.scrollIntoView();
     await dashboardPage.addressBook.click();
     await dashboardPage.telephone.setValue(phoneNumber);
